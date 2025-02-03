2e89a56fa0e41c695e26f8960519ae858c513374
Cipster93 <stroeflorinc@yahoo.com>
2025-02-03 23:58:26 +0200

    Credentials

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,56 +1,99 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import Credentials from "next-auth/providers/credentials";
 
 import { db } from "@/server/db";
+import { randomUUID } from "crypto";
+import { verifyPassword } from "./utils";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
+      name: string;                              //Aici definesti user-ul. Adica cel pe care il ai in schmea din Prisma
+      email: string;
+      password: string;
+      image: string | null;
+      role: "WRITER" | "EDITOR" | "ADMIN";
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
-export const authConfig = {
+export const authConfig = {                                        // Definesti credentialele pentru log in, aici cu email si password, insa le poti folosi pentru orice autentificare ai nevoie
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { lable: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          throw new Error("No credentials provider");
+        }
+        const { email, password } = credentials;
+        const existingUser = await db.user.findFirst({           // se cauta in baza de date userul cu emailul introdus in Ui login
+          where: {
+            email: sanitiseEmail(email as string),
+          },
+        });
+
+        if (existingUser && password) {         // daca emailul si parola sunt corect introduse se returneaza userul
+          const isAnValidPassword: boolean = await verifyPassword(     // se verifica parola cu parola criptata
+            existingUser.password,
+            password as string
+          )
+          if (isAnValidPassword) {
+            return existingUser;
+          } else {
+            throw new Error("Invalid User");
+          }
+        }
+        return existingUser;
+      }
+    })
+
   ],
   adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },    // JWT > json web tooken > ce face? > codifica toate datele introduse in Dumnezeu stie ce..
+  secret: process.env.AUTH_SECRET ?? randomUUID(),        // AUTH_SECRET > o gasesti in env. acolo ai cheia
+  pages: {
+    signIn: "/login",
+    newUser: "/register",
+  },
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    session: async ({ session, token }) => {         // aici sesiunea primeste tokenul dupa ce l-ai implementat
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.name = token.name!;
+        session.user.email = token.email!;
+        session.user.image = token.image as string ?? null;
+        session.user.role = token.role as "WRITER" | "EDITOR" | "ADMIN";
+      }
+      return session;
+    },
+    jwt: async ({ token, user }) => {
+      const userFromDB = await db.user.findFirst({         // functie care cauta in baza de date userul cu emailul introdus
+        where: {
+          email: token.emil!,
+        }
+      });
+      if (!userFromDB) {
+        throw new Error("User not found")
+      }
+      if (user) {                                    // daca este gasit adaugam in tooken toate infomatiile despre user cum a fost definit in schema
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.image = user.image;
+        token.role = userFromDB.role;
+      }
+
+      return token;                        // intotdeauna returnam tokenu
+    },
   },
 } satisfies NextAuthConfig;
+function sanitiseEmail(arg0: string): any {
+  throw new Error("Function not implemented.");
+}
+
