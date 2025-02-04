3653e00d32a8010b87604f83e53ffdfe9b5ca21b
razvan.todica <razvan.todica@mobiversal.com>
2025-02-04 19:10:25 +0200

    fix sanitise with sanitize from corect path

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,16 +1,15 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
 import { db } from "@/server/db";
 import { randomUUID } from "crypto";
-import { verifyPassword } from "./utils";
+import { verifyPassword, sanitizeEmail } from "./utils";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      name: string;                              //Aici definesti user-ul. Adica cel pe care il ai in schmea din Prisma
+      name: string; //Aici definesti user-ul. Adica cel pe care il ai in schmea din Prisma
       email: string;
       password: string;
       image: string | null;
@@ -19,10 +18,11 @@ declare module "next-auth" {
   }
 }
 
-export const authConfig = {                                        // Definesti credentialele pentru log in, aici cu email si password, insa le poti folosi pentru orice autentificare ai nevoie
+export const authConfig = {
+  // Definesti credentialele pentru log in, aici cu email si password, insa le poti folosi pentru orice autentificare ai nevoie
   providers: [
     Credentials({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
         email: { lable: "Email", type: "text" },
         password: { label: "Password", type: "password" },
@@ -32,17 +32,20 @@ export const authConfig = {                                        // Definesti 
           throw new Error("No credentials provider");
         }
         const { email, password } = credentials;
-        const existingUser = await db.user.findFirst({           // se cauta in baza de date userul cu emailul introdus in Ui login
+        const existingUser = await db.user.findFirst({
+          // se cauta in baza de date userul cu emailul introdus in Ui login
           where: {
-            email: sanitiseEmail(email as string),
+            email: sanitizeEmail(email as string),
           },
         });
 
-        if (existingUser && password) {         // daca emailul si parola sunt corect introduse se returneaza userul
-          const isAnValidPassword: boolean = await verifyPassword(     // se verifica parola cu parola criptata
+        if (existingUser && password) {
+          // daca emailul si parola sunt corect introduse se returneaza userul
+          const isAnValidPassword: boolean = await verifyPassword(
+            // se verifica parola cu parola criptata
             existingUser.password,
-            password as string
-          )
+            password as string,
+          );
           if (isAnValidPassword) {
             return existingUser;
           } else {
@@ -50,38 +53,40 @@ export const authConfig = {                                        // Definesti 
           }
         }
         return existingUser;
-      }
-    })
-
+      },
+    }),
   ],
   adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" },    // JWT > json web tooken > ce face? > codifica toate datele introduse in Dumnezeu stie ce..
-  secret: process.env.AUTH_SECRET ?? randomUUID(),        // AUTH_SECRET > o gasesti in env. acolo ai cheia
+  session: { strategy: "jwt" }, // JWT > json web tooken > ce face? > codifica toate datele introduse in Dumnezeu stie ce..
+  secret: process.env.AUTH_SECRET ?? randomUUID(), // AUTH_SECRET > o gasesti in env. acolo ai cheia
   pages: {
     signIn: "/login",
     newUser: "/register",
   },
   callbacks: {
-    session: async ({ session, token }) => {         // aici sesiunea primeste tokenul dupa ce l-ai implementat
+    session: async ({ session, token }) => {
+      // aici sesiunea primeste tokenul dupa ce l-ai implementat
       if (token) {
         session.user.id = token.id as string;
         session.user.name = token.name!;
         session.user.email = token.email!;
-        session.user.image = token.image as string ?? null;
+        session.user.image = (token.image as string) ?? null;
         session.user.role = token.role as "WRITER" | "EDITOR" | "ADMIN";
       }
       return session;
     },
     jwt: async ({ token, user }) => {
-      const userFromDB = await db.user.findFirst({         // functie care cauta in baza de date userul cu emailul introdus
+      const userFromDB = await db.user.findFirst({
+        // functie care cauta in baza de date userul cu emailul introdus
         where: {
           email: token.emil!,
-        }
+        },
       });
       if (!userFromDB) {
-        throw new Error("User not found")
+        throw new Error("User not found");
       }
-      if (user) {                                    // daca este gasit adaugam in tooken toate infomatiile despre user cum a fost definit in schema
+      if (user) {
+        // daca este gasit adaugam in tooken toate infomatiile despre user cum a fost definit in schema
         token.id = user.id;
         token.name = user.name;
         token.email = user.email;
@@ -89,11 +94,7 @@ export const authConfig = {                                        // Definesti 
         token.role = userFromDB.role;
       }
 
-      return token;                        // intotdeauna returnam tokenu
+      return token; // intotdeauna returnam tokenu
     },
   },
 } satisfies NextAuthConfig;
-function sanitiseEmail(arg0: string): any {
-  throw new Error("Function not implemented.");
-}
-
